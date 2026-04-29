b4280c3c8a316f14e01172c0fa54df26e7a9b071
Jorix2007 <jabirbusines@gmail.com>
2026-04-30 00:54:47 +0400

    lolac

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- Script that lists all records of the table second_table.
+-- Don't list rows without a name value.
+SELECT score, name 
+FROM second_table 
+WHERE name IS NOT NULL AND name <> ''
+ORDER BY score DESC;
