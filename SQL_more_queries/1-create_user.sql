3dcd97fdf4d95c5cb734d4c583f7190fd43250e0
Jorix2007 <jabirbusines@gmail.com>
2026-05-07 09:19:06 +0400

    lolca

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,4 @@
--- Creates the user user_0d_1 with the password user_0d_1_pwd
+--  creates the MySQL server user user_0d_1. 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grants all privileges on all databases and tables to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Flushes privileges to ensure that the changes take effect immediately
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
