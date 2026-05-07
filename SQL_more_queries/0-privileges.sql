660c3b58a2eef45e2a1227bd8c458081dae4ea29
Jorix2007 <jabirbusines@gmail.com>
2026-05-07 09:06:27 +0400

    locla

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Lists all privileges for user_0d_1 on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Lists all privileges for user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
